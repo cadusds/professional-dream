4f451fed822152d50220c0c72b126c6659b8865a
cadusds <carlos.silva150995@gmail.com>
2023-05-31 03:13:17 -0300

    update

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+    )
+
+    bio = models.CharField(max_length=250,blank=True)
+
+    def __str__(self) -> str:
+        return self.user.get_username()
